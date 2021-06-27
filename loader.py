98ba44d55f882952fdb859257849888ae12c1caf
Alexander <startsevdev@gmail.com>
2021-06-27 20:04:16 +0300

    - Added connection to postgres from Docker

--- loader.py
+++ loader.py
@@ -2,10 +2,9 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
-from utils.db_api.sql import create_pool
-
+from sql import create_pool
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dispatcher = Dispatcher(bot, storage=storage)
-# database = dispatcher.loop.run_until_complete(create_pool())
+database = dispatcher.loop.run_until_complete(create_pool())
--- sql.py
+++ sql.py
@@ -2,7 +2,7 @@ import asyncio
 import asyncpg
 import logging
 
-from data.config import IP, PG_PASS, PG_USER
+from data.config import PG_HOST, PG_PASS, PG_USER
 
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
@@ -10,12 +10,12 @@ logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(
 
 
 async def create_db():
-    create_db_command = open("create_db.sql", "r").read()
+    create_db_command = open("utils/db_api/create_db.sql", "r").read()
 
     logging.info("Connecting to database...")
     conn: asyncpg.Connection = await asyncpg.connect(user=PG_USER,
                                                      password=PG_PASS,
-                                                     host=IP)
+                                                     host=PG_HOST)
     await conn.execute(create_db_command)
     await conn.close()
     logging.info("Table users created")
@@ -24,7 +24,7 @@ async def create_db():
 async def create_pool():
     return await asyncpg.create_pool(user=PG_USER,
                                      password=PG_PASS,
-                                     host=IP)
+                                     host=PG_HOST)
 
 
 if __name__ == '__main__':
