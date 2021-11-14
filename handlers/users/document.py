853cc864d84a2441b742e89ec01db3987cd3f817
aleksandr <startsevdev@gmail.com>
2021-11-14 17:20:13 +0300

    Добавил рассылку

--- handlers/users/document.py
+++ handlers/users/document.py
@@ -13,13 +13,20 @@ logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(
 
 @dispatcher.message_handler(content_types=types.ContentTypes.DOCUMENT, user_id=ADMINS)
 async def document_handler(message: types.Message):
-    if message.document.file_name[-4:] == ".csv":
+    # ПРИСЛАЛИ ПОЛЬЗОВАТЕЛЕЙ
+    if message.document.file_name == "users.csv":
+        # скачиваем файл
+        await bot.download_file_by_id(message.document.file_id, "data/users.csv")
+        logging.info("File users.csv replaced")
+        await message.answer("👥 База пользователей для рассылки обновлена")
+
+    # ПРИСЛАЛИ РЕСТОРАНЫ
+    elif message.document.file_name[-4:] == ".csv":
         # скачиваем файл
         await bot.download_file_by_id(message.document.file_id, "data/restaurants.csv")
         logging.info("File restaurants.csv replaced")
-
         # обновляем restaurants (который лежит в loader)
         restaurants_storage.update_restaurants(create_restaurants())
         logging.info("Parsing finished")
-        await message.answer("🙌 База обновлена ")
+        await message.answer("📁 База ресторанов обновлена")
 
--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,4 +1,5 @@
 import time
+import asyncio
 
 from aiogram import types, bot
 from aiogram.dispatcher import FSMContext
@@ -11,6 +12,13 @@ from data import config
 
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
+
+
+# Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
+@dispatcher.message_handler(state="*", content_types=types.ContentTypes.ANY, user_id=config.ADMINS)
+async def bot_echo_admins(message: types.Message, state: FSMContext):
+    if message.text.startswith("#рассылка") and message.text != "#рассылка":
+        await mailing(message)
 
 
 # Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
@@ -31,7 +39,7 @@ async def bot_echo_all(message: types.Message, state: FSMContext):
         await bot.send_message(config.ADMINS[1], f'#предложенное_заведение\n<b>{data.get("restaurant_name")}</b>\n{message.text}')
     else:
         await message.answer('''
-        ⚠️ Если не работает кнопка (бывает с компьютера):
+⚠️ Если не работает кнопка (бывает с компьютера):
 1. Нажмите на скрепку слева от поля ввода сообщения
 2. Выберите «Геопозиция»
 3. Отправить геопозицию (должен быть включен доступ Telegram к геопозиции)
@@ -56,30 +64,35 @@ async def bot_echo_all(message: types.Message, state: FSMContext):
 ❤️ Предложить заведение – /suggest
 
 ✉️ Вопросы и пожелания – @startsevdev''')
-
     logging.info("User {} sent other content".format(message.from_user.id))
     amplitude.log(message.from_user.id, "Other")
 
-    # РАССЫЛКА
-    # if message.text == "Ootro" and str(message.from_user.id) == config.ADMIN[0]:
-    #     cluster = [[]]
-    #     counter = 0
-    #     with open('data/users.csv', newline='') as File:
-    #         reader = csv.reader(File)
-    #         for user_id in reader:
-    #             if counter < 30:
-    #                 cluster[len(cluster)-1].append(user_id[0])
-    #                 counter += 1
-    #             else:
-    #                 counter = 0
-    #                 cluster.append([])
-    #     for c in cluster:
-    #         for user_id in c:
-    #             try:
-    #                 await bot.send_message(user_id, "<b>Веганские завтраки в течение месяца</b>\n\nСегодня стартовал фестиваль Utroo\nhttps://t.me/utroo/1103\n\nИщите заведения фестиваля на нашей карте 📍\n\nВ фестивале участвуют 7 замечательных ресторанов («Мечтатели», Atlas bistro, Grecco, Tureckiy bistro, Merula на Невском, Bilbao и «Мойка 3»), а также петербургская сеть кондитерских «Британские пекарни» (а это 22 кафе по всему городу).\n\nЗавтраки без использования продуктов животного происхождения можно будет пробовать целый месяц. И речь – не о скучных кашах, но о необычных блюдах, в числе которых шоколадные панкейки с карамелью из комбучи, томаты со страчателлой из кешью, хашбраун из тыквы и веганский брауни!")
-    #                 logging.info("Sent to " + user_id)
-    #             except BaseException:
-    #                 print("Ошибка про отправке сообщения рассылки")
-    #         time.sleep(1.2)
-    #     await message.answer("Рассылка отправлена")
-    # else:
+
+async def mailing(message):
+    await message.answer("Рассылка будет запущена через 1 минуту. Чтобы отменить рассылку, отправьте файл users.csv с одним словом «id»")
+    await asyncio.sleep(60)
+    text = message.text.split("\n", 1)[1]
+    cluster = [[]]
+    counter = 0
+    await message.answer("Рассылаю сообщения...")
+    with open('data/users.csv') as File:
+        reader = csv.reader(File)
+        header = next(reader)
+        for user_id in reader:
+            if counter < 30:
+                cluster[len(cluster) - 1].append(user_id[0])
+                counter += 1
+            else:
+                counter = 1
+                cluster.append([])
+                cluster[len(cluster) - 1].append(user_id[0])
+    for c in cluster:
+        for user_id in c:
+            try:
+                await bot.send_message(user_id, text)
+                logging.info("Sent to " + user_id)
+            except BaseException:
+                logging.info("Ошибка при отправке рассылки пользователю " + user_id)
+        await asyncio.sleep(0.1)
+    await message.answer("Рассылка завершена")
+    logging.info("Рассылка завершена")
